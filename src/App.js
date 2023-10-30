89ee05e3aa4c7c81ac7335c65d11a5b855ed2028
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2023-10-30 09:35:10 -0500

    add row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-  <head>App</head>
+  <h1>App</h1>
+  <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals}/>
+  <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
