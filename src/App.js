ae973075b147e65d0aade5cb6eccb13b6478dead
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2023-11-01 09:34:25 -0500

    add styling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-  <h1>App</h1>
-  <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals}/>
-  <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <h1>App</h1>
+      <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionsMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -3,7 +3,7 @@ const API_KEY = "b0db1c88a7f6de0769607d909a14a44b";
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionsMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
